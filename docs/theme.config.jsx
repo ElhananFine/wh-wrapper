64b3bf432ce3f7c2bc90cb5070f673a2d6238a40
Elhanan Fine <elhanan@elhananfine.com>
2024-03-22 13:44:40 +0200

    new

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,6 +1,14 @@
 export default {
-  logo: <span>Wh-Wrapper Documentation</span>,
-  project: {
-    link: "https://github.com/ElhananFine/wh-wrapper",
-  },
+    docsRepositoryBase: "https://github.com/ElhananFine/wh-wrapper/tree/main/docs",
+    footer: {
+        text: (
+            <a href="https://www.linkedin.com/in/elhananfine/" target="_blank">
+                --- Made with ❤️ and 🔨 by Elhanan Fine
+            </a>
+        ),
+    },
+    logo: <span>Wh-Wrapper Documentation</span>,
+    project: {
+        link: "https://github.com/ElhananFine/wh-wrapper/",
+    },
 };
